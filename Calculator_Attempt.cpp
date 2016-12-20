bd31059606d818df24b86966975e3c11db666ac3
KingsleyBawuah <bawuahk@oregonstate.edu>
2016-12-19 21:58:42 -0500

    Add files via upload
    Initial commit

---
+++ Calculator_Attempt.cpp
@@ -0,0 +1,23 @@
+// Attempt at making a basic calculator with user prompting and commands.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* Prompt user for command */
+	cout << "What would you like to do?" << endl 
+	<< "Press c for list of commands" << endl
+	<<"Press q to quit";
+
+
+
+	//Now to accept input and point towards 
+	int input;
+	int c;
+	cin >> input;
+	if (input == c)
+			cout << "a - addition";
+
+
+	return 0;
+}
