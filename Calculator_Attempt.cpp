fdb33e2d468852a6b57e1604980dbad90a6573e9
KingsleyBawuah <bawuahk@oregonstate.edu>
2016-12-21 00:34:36 -0500

    Fixed the code
    Still learning this crap

--- Calculator_Attempt.cpp
+++ Calculator_Attempt.cpp
@@ -23,54 +23,56 @@ int main()
 	//Now to accept input and point towards slected option
 	string input;
 	float first_number, second_number, product, sum, quotient;
+
 	cin >> input;
 
-        if (input == "1")
-        {
+	if (input == "1")
+	{
 
-            cout << "What's the first number?: " << endl;
-            cin >> first_number;
-            cout << "What's the second number?:" << endl;
-            cin >> second_number;
+		cout << "What's the first number?: " << endl;
+		cin >> first_number;
+		cout << "What's the second number?:" << endl;
+		cin >> second_number;
 
-            sum = first_number + second_number;
-            cout << "The sum is " << sum << endl;
-        }
+		sum = first_number + second_number;
+		cout << "The sum is " << sum << endl;
+	}
 
-            else if (input == "2")
-            {
+	else if (input == "2")
+	{
 
-                cout << "What's the first number? " << endl;
-                cin >> first_number;
-                cout << "What's the second number" << endl;
-                cin >> second_number;
+		cout << "What's the first number? " << endl;
+		cin >> first_number;
+		cout << "What's the second number" << endl;
+		cin >> second_number;
 
-                product = first_number * second_number;
-                cout << "The product is " << product << endl;
-            }
+		product = first_number * second_number;
+		cout << "The product is " << product << endl;
+	}
 
-            else if (input == "3")
-            {
+	else if (input == "3")
+	{
 
-                cout << "What's the first number? " << endl;
-                cin >> first_number;
-                cout << "What's the second number" << endl;
-                cin >> second_number;
+		cout << "What's the first number? " << endl;
+		cin >> first_number;
+		cout << "What's the second number" << endl;
+		cin >> second_number;
 
-                quotient = first_number / second_number;
-                cout << "The quotient is " << quotient << endl;
-            }
-            else if (input == "9")
-            {
-                return 0;
-            }
+		quotient = first_number / second_number;
+		cout << "The quotient is " << quotient << endl;
+	}
+	else if (input == "9")
+	{
+		return 0;
+	}
 
 
-        else
-        {
-            cout << "That is not a recognized command!";
+	else
+	{
+		cout << "That is not a recognized command!";
 
 	}
-	
-	
-	
+
+
+	return 0;
+}
